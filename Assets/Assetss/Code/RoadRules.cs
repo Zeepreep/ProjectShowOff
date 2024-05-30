9d2a9597fe5c812159d905080497d2ab4501e0a7
Calvin Tjoaquinn <66313400+calvin123-gntng@users.noreply.github.com>
2024-05-30 21:56:22 +0200

    pedestrians
    pedestrians

---
+++ Assets/Assetss/Code/RoadRules.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadRules : MonoBehaviour {
+
+	//the intersection robot system
+
+	//I guess amber means (In my opinion) if possible you should stop, if too late, pass carefully. dont rush to pass the amber before it turns red, 
+	//i have actually seen a bunch of horrible crashes because of that mistake, one of them right in front of me. sounded like a small explosion on impact, 
+	//then tires screeching and both vehicles coming to a slow halt (T boned and stuck like that). slowly bumped into a traffic light. 
+	//the woman driving the t-boned car died in the car because of the massive impact (after a while, few minutes)
+
+		//DUDE, buzz kill. asshole!!! (i am talking about myself BTW)
+
+	public PointState xState, zState;
+
+	public enum PointState {
+		Go,
+		Ready,
+		Stop
+	}
+
+	void Start () {
+		StartCoroutine (RobotCalculation ());
+	}
+
+	void Update () {
+		if (Input.GetKey(KeyCode.R)) {
+			StopAllCoroutines ();
+			StartCoroutine (RobotCalculation ());
+		}
+	}
+
+	IEnumerator RobotCalculation () {
+		xState = PointState.Go;
+		zState = PointState.Stop;
+		yield return new WaitForSeconds (8f);
+		xState = PointState.Ready;
+		zState = PointState.Stop;
+		yield return new WaitForSeconds (4f);
+		xState = PointState.Stop;
+		zState = PointState.Go;
+		yield return new WaitForSeconds (8f);
+		xState = PointState.Stop;
+		zState = PointState.Ready;
+		yield return new WaitForSeconds (4f);
+		StartCoroutine (RobotCalculation ());
+	}
+}
---
+++ Assets/Assetss/Scripts/Pedestrians.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public abstract class Pedestrian : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float rotationSpeed = 5.0f;
+    protected Transform targetWaypoint;
+    protected int targetWaypointIndex = 0;
+    protected WaypointManager waypointManager;
+
+    protected virtual void Start()
+    {
+        waypointManager = FindObjectOfType<WaypointManager>();
+        if (waypointManager != null && waypointManager.waypoints.Length > 0)
+        {
+            targetWaypoint = waypointManager.waypoints[targetWaypointIndex];
+        }
+    }
+
+    protected virtual void Update()
+    {
+        if (targetWaypoint != null)
+        {
+            MoveTowardsTarget();
+        }
+    }
+
+    protected void MoveTowardsTarget()
+    {
+        Vector3 direction = (targetWaypoint.position - transform.position).normalized;
+        float step = speed * Time.deltaTime;
+
+        // Move towards the target
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, step);
+
+        // Rotate towards the target
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        Quaternion adjustedRotation = targetRotation * Quaternion.Euler(GetForwardFacingRotation());
+        transform.rotation = Quaternion.Slerp(transform.rotation, adjustedRotation, rotationSpeed * Time.deltaTime);
+
+        // Check if the pedestrian is close to the target waypoint
+        if (Vector3.Distance(transform.position, targetWaypoint.position) < 0.1f)
+        {
+            targetWaypointIndex = (targetWaypointIndex + 1) % waypointManager.waypoints.Length;
+            targetWaypoint = waypointManager.waypoints[targetWaypointIndex];
+        }
+    }
+
+    protected abstract Vector3 GetForwardFacingRotation();
+}
