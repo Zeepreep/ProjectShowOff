4e9ae7fda06966654856baf30af6f2abab5efe16
Calvin Tjoaquinn <66313400+calvin123-gntng@users.noreply.github.com>
2024-06-24 23:57:42 +0200

    traffic system added

--- Assets/Assetss/Code/RoadPoint.cs
+++ Assets/Assetss/Code/RoadPoint.cs
@@ -1,28 +1,121 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RoadPoint : MonoBehaviour
 {
+
+    //This is the actual point the vehicle follow
+
+    //NEXT CLASS!!!!
+
+    public List<RoadPoint> connectedPoints = new List<RoadPoint>();
+    public StateAxis robotAxis;
+    public PointType pointType;
+    public State state;
+    public bool go;
+    public int speed;
+
+    private RoadRules.PointState roadState;
+    private RoadRules roadRules;
+
+    public enum State
+    {
+        In,
+        Out,
+        Middle
+    }
+
+    public enum StateAxis
+    {
+        None,
+        XRobot,
+        ZRobot
+    }
+
     public enum PointType
     {
-        Normal,
-        StoppingPoint
+        None,
+        StoppingPoint,
     }
 
-    public PointType pointType;
-    public float speed;
-
-    public List<RoadPoint> connectedPoints = new List<RoadPoint>(); // List to store connected points
-
-    // Method to add a connected point
-    public void AddConnectedPoint(RoadPoint point)
+    void Awake()
     {
-        connectedPoints.Add(point);
+        roadRules = GameObject.FindGameObjectWithTag("RoadRules").GetComponent<RoadRules>();
     }
 
-    // Method to remove a connected point
-    public void RemoveConnectedPoint(RoadPoint point)
+    void Update()
     {
-        connectedPoints.Remove(point);
+        if (transform.parent.localEulerAngles.y == 0 || transform.parent.localEulerAngles.y == 180)
+        {
+            if (robotAxis == StateAxis.XRobot)
+            {
+                roadState = roadRules.xState;
+            }
+            else if (robotAxis == StateAxis.ZRobot)
+            {
+                roadState = roadRules.zState;
+            }
+        }
+        else
+        {
+            if (robotAxis == StateAxis.XRobot)
+            {
+                roadState = roadRules.zState;
+            }
+            else if (robotAxis == StateAxis.ZRobot)
+            {
+                roadState = roadRules.xState;
+            }
+        }
+        if (roadState == RoadRules.PointState.Go)
+        {
+            go = true;
+        }
+        else
+        {
+            go = false;
+        }
+        connectedPoints = connectedPoints.Distinct().ToList();
+    }
+
+    void OnDrawGizmos()
+    {
+        if (pointType == PointType.StoppingPoint)
+        {
+            if (roadState == RoadRules.PointState.Go)
+                Gizmos.color = Color.green;
+            else if (roadState == RoadRules.PointState.Ready)
+                Gizmos.color = Color.yellow;
+            else if (roadState == RoadRules.PointState.Stop)
+                Gizmos.color = Color.red;
+            if (connectedPoints.Count > 0)
+            {
+                for (int i = 0; i < connectedPoints.Count; i++)
+                {
+                    if (connectedPoints[i] != null)
+                    {
+                        Gizmos.DrawLine(transform.position, connectedPoints[i].transform.position);
+                    }
+                }
+            }
+            Gizmos.DrawWireSphere(transform.position, 0.4f);
+        }
+        else if (pointType == PointType.None)
+        {
+            Gizmos.color = Color.cyan;
+            if (connectedPoints.Count > 0)
+            {
+                for (int i = 0; i < connectedPoints.Count; i++)
+                {
+                    if (connectedPoints[i] != null)
+                    {
+                        Gizmos.DrawLine(transform.position, connectedPoints[i].transform.position);
+                    }
+                }
+            }
+            Gizmos.DrawWireSphere(transform.position, 0.4f);
+        }
     }
 }
